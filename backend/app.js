6822f29e4ed2d07ffc885a64f5117e97ce232c85
fletcher <bigpanel@gmail.com>
2024-01-24 19:05:59 +0100

    momgoose

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser")
 const app = express();
 
+const Post = require('./models/post')
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false})) //only standard encoding
 
@@ -15,7 +17,11 @@ app.use((req,res, next)=>{
 })
 
 app.post("/api/posts", (req, res, next) => {
-    const post = req.body
+    //const post = req.body
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    })
     console.log(post)
     res.status(201).json({
         message: 'Post added'
